3a622d976142e714feb01fb6192b4180ef06ca3b
sanath777 <sanathgts@gmail.com>
2020-06-21 19:46:36 +0530

    Updates

---
+++ precache-manifest.126604086b0b49a75dc05c6c257e1582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43ace8d35bb201d1639ca315ef5e204",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "426a70313796634e562e",
+    "url": "/react-deploy/static/js/2.c4388139.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.c4388139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dfd61249573fd674933",
+    "url": "/react-deploy/static/js/main.543806a9.chunk.js"
+  },
+  {
+    "revision": "da0870fb64f99a520609",
+    "url": "/react-deploy/static/js/runtime-main.af7c1b73.js"
+  }
+]);
